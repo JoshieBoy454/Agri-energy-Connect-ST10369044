c5bba2b7181f808e9856a6361cfe3bf0761e85ee
Joshua <Joshuagreggain@gmail.com>
2025-05-13 00:16:14 +0200

    Add project files.

---
+++ Agri-energy Connect ST10369044/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Agri_energy_Connect_ST10369044.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
