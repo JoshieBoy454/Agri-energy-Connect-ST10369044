f23b9143b0cb425cae7da31603609b77cfb9d4e2
Joshua <Joshuagreggain@gmail.com>
2025-05-13 23:16:42 +0200

    Added Users and db Context classes

---
+++ Agri-energy Connect ST10369044/Data/AppDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Agri_energy_Connect_ST10369044.Data
+{
+    public class AppDbContext
+    {
+    }
+}
---
+++ Agri-energy Connect ST10369044/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace Agri_energy_Connect_ST10369044.Models
+{
+    public class Users : IdentityUser
+    {
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
